ff2dc21457cffcf9408615331f7899ab6825e75f
Yevheniia Dolzhykova <nikola.tesla0510@gmail.com>
2021-11-13 00:05:48 +0200

    add changes to scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import './sass/main.scss';
 import { refs } from './js/refs.js';
 import clearGallery  from './js/clear-gallery.js';
 import cardMarkup from './templates/card-markup.hbs';
-import pictureMarkup from './templates/big-picture-markup.hbs'
 import PhotosApiService from './js/photos-service.js';
 import {addLoadMoreButton, removeLoadMoreButton} from './js/toggle-loadmore-button.js';
 import smoothScroll from './js/scroll-downloading-picture.js';
@@ -12,7 +11,6 @@ const photosApiService = new PhotosApiService();
 
 refs.searchForm.addEventListener('submit', onSearchPhotos);
 refs.loadMoreButton.addEventListener('click', onButtonClickLoadMore);
-
 
 function onSearchPhotos(event) {
     event.preventDefault();
@@ -35,33 +33,13 @@ function onButtonClickLoadMore() {
  }
 
 function appendPhotosMarkup(photos) {
+   
     const listCards = cardMarkup(photos);
-    
     photos.length === 12 ? addLoadMoreButton() : removeLoadMoreButton();
-  
     refs.gallery.insertAdjacentHTML('beforeend', listCards);
     smoothScroll();
     
-
 }
 
-
-
-//  function onImageClick(event) {
-//   event.preventDefault();
-//   if (event.target.nodeName !== 'IMG') {
-//     return;
-//   }
-  
-//   refs.modalRef.classList.add('is-open');
-//   refs.lightBoxImageRef.src = event.target.dataset.source;
-//   console.log(refs.lightBoxImageRef);
-     
-// }
-
-// function onBtnClick(event) {
-//   refs.modalRef.classList.remove('is-open');
-//   refs.lightBoxImageRef.src = '';
-// }
-    
+ 
 
